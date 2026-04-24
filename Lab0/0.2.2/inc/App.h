080dcb65cfb80d5a9ce261d956dc5582c884546c
Lukkee <lukas@holy.nu>
2026-04-24 08:31:59 +0200

    Fixed: Syntax Issue in App.h

--- Lab0/0.2.2/inc/App.h
+++ Lab0/0.2.2/inc/App.h
@@ -10,12 +10,7 @@ typedef struct {
   char buffer[12];
   int cnt;
   int indices[32];
-<<<<<<< HEAD
   double periods[32];
-
-=======
-  double periods[25];
->>>>>>> bb35828722915874f4d72624aa3dab83ca704cbb
 } App;
 
 #define initApp()                                                              \
