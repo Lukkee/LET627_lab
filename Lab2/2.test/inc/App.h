96a220c3fbabd10cc732ef44108bf8c60f3a08dd
Lukkee <lukas@holy.nu>
2026-05-08 14:52:24 +0200

    Added: methods for 2.x

--- Lab2/2.test/inc/App.h
+++ Lab2/2.test/inc/App.h
@@ -5,6 +5,23 @@
 
 #define DAC_DATA ((volatile uint8_t *)0x4000741C)
 
+/* PARAMETRAR */
+#define MAX_TEMPO 240
+#define MIN_TEMPO 60
+#define MAX_KEY 5
+#define MIN_KEY -5
+#define MAX_VOL 20
+#define MIN_VOL 0
+
+/* KONTROLLER */
+#define TEMPOKEY    't'
+#define KEYKEY      'k'
+#define MUTEKEY     'm'
+#define VOLUPKEY    '+'
+#define VOLDOWNKEY  '-'
+#define CANCELKEY   'c'
+#define HELPKEY     'h'
+
 typedef struct {
   Object super;
   int cnt;
@@ -36,13 +53,21 @@ typedef struct {
 #define initToneGenerator()                                                              \
   { initObject(), 0, 3, 1203, 1, 0 }
 
+/* APP */
 void reader(App *, int);
 void receiver(App *, int);
 void startApp(App *, int);
 
+/* TONEGENERATOR */
 void toneGenerator(ToneGenerator *, int);
 void silence(ToneGenerator *, int);
 void setTone(ToneGenerator *, int);
+void setVolume(ToneGenerator *, int);
+
+
+/* MUSICPLAYER */
 void playNote(MusicPlayer *, int);
+void setTempo(MusicPlayer *, int);
+void setKey(MusicPlayer *, int);
 
 #endif
