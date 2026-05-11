3e7df3b40eea96df92966f3dbb1d6e30937220f6
Lukkee <lukas@holy.nu>
2026-05-11 10:17:41 +0200

    Added: comments to Lab2

--- Lab2/MusicPlayer/src/App.c
+++ Lab2/MusicPlayer/src/App.c
@@ -12,10 +12,11 @@ extern ToneGenerator tg;
 extern Can can0;
 extern Serial sci0;
 
+/* Frekvens "indices" */
 static int frequencies[]  = {0,2,4,0,0,2,4,0,4,5,7,4,5,7,7,9,
                              7,5,4,0,7,9,7,5,4,0,0,-5,0,0,-5,0};
 
-// Sparas som *2 för att ej behöva använda float
+// Notlängder sparas som *2 för att ej behöva använda float
 static int lengths[]      = {2,2,2,2,2,2,2,2,2,2,4,2,2,4,1,1,
                              1,1,2,2,1,1,1,1,2,2,2,2,4,2,2,4};
 
@@ -27,24 +28,29 @@ void receiver(App *self, int unused) {
 }
 
 void reader(App *self, int c) {
-
+  /* Hantera input */
   switch (c) {
+    /* Direkta handlingar */
     case MUTEKEY:     tg.mute = !tg.mute; break;
     case VOLUPKEY:    ASYNC(&tg, setVolume, tg.volume + 1); break;
     case VOLDOWNKEY:  ASYNC(&tg, setVolume, tg.volume - 1); break;
 
-    default: /* Integers */
-      if ( c != CANCELKEY && c != TEMPOKEY && c != KEYKEY && self->cnt < 11) { /* FORTSÄTT SKRIVA */
+    default: /* Integer input */
+
+    if ( c != CANCELKEY && c != TEMPOKEY && c != KEYKEY && self->cnt < 11) { /* FORTSÄTT SKRIVA */
         self->buffer[self->cnt] = c;    // spara char till buffer[cnt]
         self->cnt++;                    // öka cnt
       }
+
       else { /* SLUTA SKRIVA */
+
         self->buffer[self->cnt] = '\0';                 // null-terminator
         int num = atoi(self->buffer);                   // ASCII to Integer(buffer)
+
         self->cnt = 0;                                  // Nollställ cnt
         memset(self->buffer, 0, sizeof(self->buffer));  // Rensa buffer
 
-        switch (c) {
+        switch (c) { /* Vad ska skickas */
           case TEMPOKEY:  ASYNC(&mp, setTempo, num); break;
           case KEYKEY:    ASYNC(&mp, setKey, num); break;
           default:  break;
@@ -61,6 +67,7 @@ void toneGenerator(ToneGenerator *self, int arg) {
     *DAC_DATA = 0;
   }
 
+  // Sparar msg för att kunna avbryta vid silence()
   self->pending = SEND(USEC(self->period), USEC(10), self, toneGenerator, 0);
 }
 
@@ -69,6 +76,7 @@ int getPeriods(int index) {
                           1275, 1203, 1136, 1072, 1012, 955, 901, 851,
                           803, 758, 715, 675, 637, 601, 568, 536, 506};
 
+  // Indexera med +10 för att räkna med key som parsas i index
   return periods[index + 10];
 }
 
@@ -99,24 +107,25 @@ void setVolume(ToneGenerator *self, int arg) {
 void setTone(ToneGenerator *self, int arg) {
   self->period = arg;
   self->silence = 0;
+  // Om toneGenerator inte finns i kön
   if (!self->pending) self->pending = SEND(USEC(self->period), USEC(10), self, toneGenerator, 0);
 }
 
 void silence(ToneGenerator *self, int arg) {
   self->silence = 1;
-  if (self->pending) {
-  ABORT(self->pending);
-  self->pending = NULL;
+  if (self->pending) { // Om call redan är i kö, ta bort, avbryt
+    ABORT(self->pending);
+    self->pending = NULL;
   }
 }
 
 void playNote(MusicPlayer *self, int arg) {
   // räknar massa skit
-  int i = self->index;
-  Time beat = MSEC(60000 / self->tempo);
-  Time note_time = beat * lengths[i] / 2;
-  Time gap_time = MSEC(50);
-  Time play_time = note_time - gap_time;
+  int i           = self->index;
+  Time beat       = MSEC(60000 / self->tempo);
+  Time note_time  = beat * lengths[i] / 2;
+  Time gap_time   = MSEC(50);
+  Time play_time  = note_time - gap_time;
 
   // beat = 60s / bpm
   // note_time = beat * notlängd
@@ -125,20 +134,21 @@ void playNote(MusicPlayer *self, int arg) {
   // next_start = 10us (dl för setTone) + note_time
   // next_stop = beat * (nästa notlängd) - gap_time
 
+  // Räkna tider för nästa not
   Time next_start = USEC(10) + note_time;
   Time next_stop = beat * (lengths[(i + 1) % 32] / 2) - gap_time;
 
+  // Skapa kö
   SEND(0, USEC(10), &tg, setTone, getPeriods(frequencies[i] + self->key));
   SEND(play_time, gap_time, &tg, silence, 0);
   self->index = (self->index + 1) % 32;
   SEND(next_start, next_stop, &mp, playNote, 0);
 }
 
-
 void startApp(App *self, int arg) {
   CAN_INIT(&can0);
   SCI_INIT(&sci0);
-  SCI_WRITE(&sci0, "Cool j\x84vla musikspelare\n");
+  SCI_WRITE(&sci0, "Cool musikspelare\n");
 
   ASYNC(&mp, playNote, 0);
 }
